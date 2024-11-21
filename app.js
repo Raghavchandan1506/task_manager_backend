d6fe779fe165092193f6abf8e614d28e618ea30b
Raghav Chandan <raghavchandan1715@gmail.com>
2024-11-21 10:54:17 +0530

    cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
-
-
-import userRouter from "./Routes/TaskRouter.js"
+import userRouter from "./Routes/TaskRouter.js";
 import bodyParser from "body-parser";
-import cors from 'cors'
+import cors from "cors";
 app.use(bodyParser.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 app.use("/api/v1/Todos", userRouter);
 export { app };
